7980b15d37e47fec75cf5c8e7414021f94ca7334
Digwijay <digwijay2@gmail.com>
2024-08-23 00:21:32 +0530

    ..

--- src/Components/Designs.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Designs = () => {
-  return (
-    <div className='h-screen bg-gray-800 pt-16' id="design">Designs</div>
-  )
-}
-
-export default Designs
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,18 @@
 
-import Designs from '../Components/Designs'
+import AboutMe from '../Components/AboutMe/AboutMe'
+import Designs from '../Components/Designs/Designs'
 import Hero from '../Components/Hero/Hero'
 import Navbar from '../Components/Navbar/Navbar'
-
+import Skill from '../Components/Skill/Skill'
 const Home = () => {
   return (
     <div className="bg-slate-950 text-yellow-50">
     <Navbar/>
     <Hero/>
+    <AboutMe/>
     <Designs/>
+    <Skill/>
+    
     </div>
   )
 }
