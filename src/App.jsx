cb3f5c613d4b94367eaccc1e98946e0c256cd940
Digwijay <digwijay2@gmail.com>
2024-10-11 23:54:49 +0530

    Set up firebase and deploy the web

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./Pages/Home"
-
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import { initializeApp } from "firebase/app";
+import { useEffect } from "react";
 
 const App = () => {
+
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDV26goxh0aWjXBjEdW5JPXgrs7e1cVImk",
+      authDomain: "ejasahammed-f10aa.firebaseapp.com",
+      projectId: "ejasahammed-f10aa",
+      storageBucket: "ejasahammed-f10aa.appspot.com",
+      messagingSenderId: "409633886446",
+      appId: "1:409633886446:web:cfe03412ce0ba9ff6f63ff",
+    };
+    
+    initializeApp(firebaseConfig);
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
-
+      <Route path="/" element={<Home />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
