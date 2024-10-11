41fd182ab61267fa938cabd217386c8fb8f49a7d
Digwijay <digwijay2@gmail.com>
2024-10-11 22:27:47 +0530

    updated designs to Coming Soon

--- src/Components/Designs/Designs.jsx
+++ src/Components/Designs/Designs.jsx
@@ -1,11 +1,13 @@
 
 const Designs = () => {
+
+  // pt-28
   return (
-    <div className="pt-28 px-4 md:px-4 lg:px-10 pb-14 grid lg:grid-cols-2 gap-6 " id="design"> 
+    <div className="pt-14 px-4 md:px-4  pb-14 grid lg:grid-cols-2 gap-6 " id="design"> 
         <div >
       
     </div>
-    <div> div lsjbfjsbv</div>
+    <div> <h2 className="text-yellow-400 text-xl">Coming soon...</h2></div>
     </div>
   );
 };
