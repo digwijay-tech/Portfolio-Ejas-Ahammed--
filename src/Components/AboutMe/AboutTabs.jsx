336f4f92bdbb61807be8007e8b670aeec87c85de
digwijay-tech <digwijay2@gmail.com>
2025-04-04 21:41:54 +0530

    Refactor AboutTabs and Skill components for improved readability and functionality; add animated progress bars and enhance CV content.

--- src/Components/AboutMe/AboutTabs.jsx
+++ src/Components/AboutMe/AboutTabs.jsx
@@ -1,8 +1,8 @@
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import { useState } from 'react';
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import { useState } from "react";
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -29,7 +29,7 @@ CustomTabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
@@ -41,20 +41,20 @@ export default function AboutTabs() {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: "100%" }}>
       <Box>
         <Tabs
           value={value}
           onChange={handleChange}
           aria-label="basic tabs example"
           sx={{
-            '& .MuiTabs-flexContainer': {
-              flexWrap: 'wrap', // Allows tabs to wrap if there is not enough space
+            "& .MuiTabs-flexContainer": {
+              flexWrap: "wrap", // Allows tabs to wrap if there is not enough space
             },
           }}
           TabIndicatorProps={{
             sx: {
-              bgcolor: '#ffeb3b', // Color of the indicator line
+              bgcolor: "#ffeb3b", // Color of the indicator line
             },
           }}
         >
@@ -62,18 +62,17 @@ export default function AboutTabs() {
             label="Education"
             {...a11yProps(0)}
             sx={{
-              '&.Mui-selected': {
-                color: '#fff',
-               
+              "&.Mui-selected": {
+                color: "#fff",
               },
-              '&': {
-                color: '#ffeb3b',
+              "&": {
+                color: "#ffeb3b",
               },
-              '&:hover': {
-                color: '#fff',
+              "&:hover": {
+                color: "#fff",
               },
-              '@media (max-width: 600px)': {
-                fontSize: '0.740rem', // Smaller font size on small screens
+              "@media (max-width: 600px)": {
+                fontSize: "0.740rem", // Smaller font size on small screens
               },
             }}
           />
@@ -81,18 +80,17 @@ export default function AboutTabs() {
             label="software"
             {...a11yProps(1)}
             sx={{
-              '&.Mui-selected': {
-                color: '#fff',
-                
+              "&.Mui-selected": {
+                color: "#fff",
               },
-              '&': {
-                color: '#ffeb3b',
+              "&": {
+                color: "#ffeb3b",
               },
-              '&:hover': {
-                color: '#fff',
+              "&:hover": {
+                color: "#fff",
               },
-              '@media (max-width: 600px)': {
-                fontSize: '0.740rem',
+              "@media (max-width: 600px)": {
+                fontSize: "0.740rem",
               },
             }}
           />
@@ -100,135 +98,227 @@ export default function AboutTabs() {
             label="Experience"
             {...a11yProps(2)}
             sx={{
-              '&.Mui-selected': {
-                color: '#fff',
-                
+              "&.Mui-selected": {
+                color: "#fff",
               },
-              '&': {
-                color: '#ffeb3b',
+              "&": {
+                color: "#ffeb3b",
               },
-              '&:hover': {
-                color: '#fff',
+              "&:hover": {
+                color: "#fff",
               },
-              '@media (max-width: 600px)': {
-                fontSize: '0.740rem',
+              "@media (max-width: 600px)": {
+                fontSize: "0.740rem",
               },
             }}
           />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-      <ul style={{ listStyleType: 'none' }}>
-          <li style={{ position: 'relative', paddingLeft: '1.5rem', paddingBottom: '0.5rem', marginBottom: '0.5rem' }}>
-            <span style={{
-              position: 'absolute',
-              left: 0,
-              top: '50%',
-              
-              transform: 'translateY(-50%)',
-              fontSize: '1.25rem', 
-              color: '#ffeb3b', 
-            }}>
+      <span className="h-60  block">
+        <ul style={{ listStyleType: "none" }}>
+          <li
+            style={{
+              position: "relative",
+              paddingLeft: "1.5rem",
+              paddingBottom: "0.5rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            <span
+              style={{
+                position: "absolute",
+                left: 0,
+                top: "50%",
+
+                transform: "translateY(-50%)",
+                fontSize: "1.25rem",
+                color: "#ffeb3b",
+              }}
+            >
               &rarr;
             </span>
-            Diploma In Digital Image <br/>
-G-Tec Computer Center
+            Diploma In Digital Image <br />
+            G-Tec Computer Center
           </li>
-          <li style={{ position: 'relative', paddingLeft: '1.5rem',paddingBottom: '0.5rem', marginBottom: '0.5rem'  }}>
-            <span style={{
-              position: 'absolute',
-              left: 0,
-              top: '50%',
-              transform: 'translateY(-50%)',
-              fontSize: '1.25rem', 
-              color: '#ffeb3b', 
-            }}>
+          <li
+            style={{
+              position: "relative",
+              paddingLeft: "1.5rem",
+              paddingBottom: "0.5rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            <span
+              style={{
+                position: "absolute",
+                left: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+                fontSize: "1.25rem",
+                color: "#ffeb3b",
+              }}
+            >
               &rarr;
             </span>
-            Diploma In Accounting <br/>
-Sobha Institute, Vadakara
+            Diploma In Accounting <br />
+            Sobha Institute, Vadakara
           </li>
-          <li style={{ position: 'relative', paddingLeft: '1.5rem', marginBottom: '0.5rem'  }}>
-            <span style={{
-              position: 'absolute',
-              left: 0,
-              top: '50%',
-              transform: 'translateY(-50%)',
-              fontSize: '1.25rem', 
-              color: '#ffeb3b', 
-            }}>
+          <li
+            style={{
+              position: "relative",
+              paddingLeft: "1.5rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            <span
+              style={{
+                position: "absolute",
+                left: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+                fontSize: "1.25rem",
+                color: "#ffeb3b",
+              }}
+            >
               &rarr;
             </span>
-            Higher Secondary <br/>
-GVHSS Madapply
+            Higher Secondary <br />
+            GVHSS Madapply
           </li>
-          </ul>
+        </ul>
+        </span>
       </CustomTabPanel>
 
       <CustomTabPanel value={value} index={1}>
-         <ul style={{ listStyleType: 'none' }}>
-          <li style={{ position: 'relative', paddingLeft: '1.5rem', marginBottom: '0.5rem' }}>
-            <span style={{
-              position: 'absolute',
-              left: 0,
-              top: '50%',
-              transform: 'translateY(-50%)',
-              fontSize: '1.25rem', 
-              color: '#ffeb3b', 
-            }}>
+      <span className="h-60  block">
+        <ul style={{ listStyleType: "none" }}>
+          <li
+            style={{
+              position: "relative",
+              paddingLeft: "1.5rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            <span
+              style={{
+                position: "absolute",
+                left: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+                fontSize: "1.25rem",
+                color: "#ffeb3b",
+              }}
+            >
               &rarr;
             </span>
             Adobe Photoshop
           </li>
-          <li style={{ position: 'relative', paddingLeft: '1.5rem', marginBottom: '0.5rem' }}>
-            <span style={{
-              position: 'absolute',
-              left: 0,
-              top: '50%',
-              transform: 'translateY(-50%)',
-              fontSize: '1.25rem', 
-              color: '#ffeb3b', 
-            }}>
+          <li
+            style={{
+              position: "relative",
+              paddingLeft: "1.5rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            <span
+              style={{
+                position: "absolute",
+                left: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+                fontSize: "1.25rem",
+                color: "#ffeb3b",
+              }}
+            >
               &rarr;
             </span>
             Adobe Illustrator
           </li>
-          <li style={{ position: 'relative', paddingLeft: '1.5rem', marginBottom: '0.5rem' }}>
-            <span style={{
-              position: 'absolute',
-              left: 0,
-              top: '50%',
-              transform: 'translateY(-50%)',
-              fontSize: '1.25rem', 
-              color: '#ffeb3b', 
-            }}>
+          <li
+            style={{
+              position: "relative",
+              paddingLeft: "1.5rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            <span
+              style={{
+                position: "absolute",
+                left: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+                fontSize: "1.25rem",
+                color: "#ffeb3b",
+              }}
+            >
               &rarr;
             </span>
             CorelDraw
           </li>
-          <li style={{ position: 'relative', paddingLeft: '1.5rem', marginBottom: '0.5rem' }}>
-            <span style={{
-              position: 'absolute',
-              left: 0,
-              top: '50%',
-              transform: 'translateY(-50%)',
-              fontSize: '1.25rem', 
-              color: '#ffeb3b', 
-            }}>
+          <li
+            style={{
+              position: "relative",
+              paddingLeft: "1.5rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            <span
+              style={{
+                position: "absolute",
+                left: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+                fontSize: "1.25rem",
+                color: "#ffeb3b",
+              }}
+            >
               &rarr;
             </span>
             InDesign
           </li>
-          </ul>
+        </ul>
+        </span>
       </CustomTabPanel>
 
       <CustomTabPanel value={value} index={2}>
-         <span>
-         Freebirds Advertising <br />
-June 2022 - 2024 July 
+        <span className="h-60 overflow-y-auto block">
+          <div>
+            <b>Graphic Designer</b> <br />
+            The Fanto Technolab · Freelance <br />
+            Dec 2024 - Mar 2025 · 4 mos <br />
+            Kozhikode, Kerala, India · Remote
+          </div>
 
-<p className='pt-8'> I am an accomplished graphic designer with 1+ years of hands-on experience in an advertising company.</p>
-         </span>
+          <div className="pt-4">
+            <b>Graphic Designer</b> <br />
+            FREEBIRD · Full-time <br />
+            Feb 2023 - Aug 2024 · 1 yr 7 mos <br />
+            Kozhikode, Kerala, India · Remote
+          </div>
+
+          <div className="pt-4">
+            <b>Graphic Designer</b> <br />
+            Be-Worldwide · Internship <br />
+            Dec 2022 - Feb 2023 · 3 mos
+          </div>
+
+          <div className="pt-4">
+            <b>Graphic Designer</b> <br />
+            Thakkol Creatives · Internship <br />
+            Sep 2022 - Nov 2022 · 3 mos
+          </div>
+
+          <div className="pt-4">
+            <b>Graphic Designer</b> <br />
+            Freebirds Advertising <br />
+            June 2022 - July 2024 <br />
+            <p className="pt-8">
+              I am an accomplished graphic designer with 1+ years of hands-on
+              experience in an advertising company.
+            </p>
+          </div>
+        </span>
       </CustomTabPanel>
     </Box>
   );
--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -1,10 +1,86 @@
+import React, { useState, useEffect, useRef } from "react";
 
-import { ProgressBarContainer as ProgressBar } from 'progress_bar_for_react';
-import { useState } from 'react';
+const AnimatedProgressBar = ({
+  percentage = 0,
+  fillerExtraStyles = {},
+  progressBarExtraStyles = {},
+  isVisible = false,
+}) => {
+  const [width, setWidth] = useState(0);
 
+  useEffect(() => {
+    // Only animate when the component becomes visible
+    if (isVisible) {
+      // Start with width at 0
+      setWidth(0);
+
+      // After a small delay, animate to the target percentage
+      const timer = setTimeout(() => {
+        setWidth(percentage);
+      }, 100);
+
+      return () => clearTimeout(timer);
+    } else {
+      // Reset width when not visible
+      setWidth(0);
+    }
+  }, [percentage, isVisible]);
+
+  const fillerStyles = {
+    height: "100%",
+    width: `${width}%`,
+    transition: "width 1.5s ease-in-out",
+    borderRadius: "inherit",
+    ...fillerExtraStyles,
+  };
+
+  const progressBarStyles = {
+    height: "20px",
+    width: "100%",
+    backgroundColor: "#e0e0de",
+    borderRadius: "50px",
+    ...progressBarExtraStyles,
+  };
+
+  return (
+    <div style={progressBarStyles}>
+      <div style={fillerStyles}>
+        <span className="text-white text-xs font-semibold ml-2">{`${percentage}%`}</span>
+      </div>
+    </div>
+  );
+};
 
 const Skill = () => {
-  const [result, setResult] = useState("") ;
+  const [result, setResult] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+  const skillSectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update state when intersection status changes
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null, // Use the viewport as the root
+        rootMargin: "0px",
+        threshold: 0.2, // Trigger when 20% of the element is visible
+      }
+    );
+
+    if (skillSectionRef.current) {
+      observer.observe(skillSectionRef.current);
+    }
+
+    // Cleanup observer
+    return () => {
+      if (skillSectionRef.current) {
+        observer.unobserve(skillSectionRef.current);
+      }
+    };
+  }, []);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -13,7 +89,7 @@ const Skill = () => {
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
-      body: formData
+      body: formData,
     });
 
     const data = await response.json();
@@ -26,95 +102,130 @@ const Skill = () => {
       setResult(data.message);
     }
   };
+
   return (
-    <div className='bg-gray-800 ' id='skill'>
-    <div className="  grid lg:grid-cols-2 lg:mx-14 mx-7 pb-10 pt-28 lg:pt-16"  >
-      
-    <div className='max-w-xl mb-2'>
-    <div className="flex lg:justify-start justify-center pb-4 ">
-        <h2 className="text-yellow-400 font-bold text-3xl ">Skill</h2>
-    </div>
-     <div className=''>
-    <h2 className='lg:text-lg font-semibold mb-1' >
-       Poster Design
-    </h2>
-    <ProgressBar percentage={85}  fillerExtraStyles={{backgroundColor:'#facc15',}} progressBarExtraStyles={{backgroundColor:'#d1d5db', height:'25px' ,width:'100%'}}/>
+    <div className="bg-gray-800" id="skill" ref={skillSectionRef}>
+      <div className="grid lg:grid-cols-2 lg:mx-14 mx-7 pb-10 pt-28 lg:pt-16">
+        <div className="max-w-xl mb-2">
+          <div className="flex lg:justify-start justify-center pb-4">
+            <h2 className="text-yellow-400 font-bold text-3xl">Skills</h2>
+          </div>
+
+          <div className="">
+            <h2 className="lg:text-lg font-semibold mb-1">Poster Design</h2>
+            <AnimatedProgressBar
+              percentage={85}
+              fillerExtraStyles={{ backgroundColor: "#facc15" }}
+              progressBarExtraStyles={{
+                backgroundColor: "#d1d5db",
+                height: "25px",
+                width: "100%",
+              }}
+              isVisible={isVisible}
+            />
+          </div>
+
+          <div className="pt-3">
+            <h2 className="lg:text-lg font-semibold mb-1">Print Design</h2>
+            <AnimatedProgressBar
+              percentage={85}
+              fillerExtraStyles={{ backgroundColor: "#facc15" }}
+              progressBarExtraStyles={{
+                backgroundColor: "#d1d5db",
+                height: "25px",
+                width: "100%",
+              }}
+              isVisible={isVisible}
+            />
+          </div>
+
+          <div className="pt-3">
+            <h2 className="lg:text-lg font-semibold mb-1">Logo Design</h2>
+            <AnimatedProgressBar
+              percentage={85}
+              fillerExtraStyles={{ backgroundColor: "#facc15" }}
+              progressBarExtraStyles={{
+                backgroundColor: "#d1d5db",
+                height: "25px",
+                width: "100%",
+              }}
+              isVisible={isVisible}
+            />
+          </div>
+
+          <div className="pt-3">
+            <h2 className="lg:text-lg font-semibold mb-1">Brand Design</h2>
+            <AnimatedProgressBar
+              percentage={80}
+              fillerExtraStyles={{ backgroundColor: "#facc15" }}
+              progressBarExtraStyles={{
+                backgroundColor: "#d1d5db",
+                height: "25px",
+                width: "100%",
+              }}
+              isVisible={isVisible}
+            />
+          </div>
+
+          <div className="pt-3">
+            <h2 className="lg:text-md font-semibold mb-1">Advertising</h2>
+            <AnimatedProgressBar
+              percentage={80}
+              fillerExtraStyles={{ backgroundColor: "#facc15" }}
+              progressBarExtraStyles={{
+                backgroundColor: "#d1d5db",
+                height: "25px",
+                width: "100%",
+              }}
+              isVisible={isVisible}
+            />
+          </div>
+        </div>
+
+        <div className="w-full md:mx-2 p-6 border mt-9 mb-5 lg:mt-4 border-yellow-400 rounded-lg shadow-lg">
+          <h2 className="text-xl font-bold mb-4 text-center">Contact Me</h2>
+          <form onSubmit={onSubmit}>
+            <div className="mb-4">
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                required
+                className="w-full p-3 border text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                required
+                className="w-full p-3 border text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              />
+            </div>
+
+            <div className="mb-4">
+              <textarea
+                name="message"
+                placeholder="Your Message"
+                className="w-full p-3 border text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full py-3 bg-yellow-400 text-white font-bold rounded-md"
+            >
+              Submit
+            </button>
+          </form>
+          <span className="text-green-300">{result}</span>
+        </div>
       </div>
-      <div className='pt-3'>
-    <h2 className='lg:text-lg font-semibold mb-1 '>
-    Print Design
-    </h2>
-    <ProgressBar percentage={85} fillerExtraStyles={{backgroundColor:'#facc15',}} progressBarExtraStyles={{backgroundColor:'#d1d5db', height:'25px' ,width:'100%'}} />
     </div>
-    <div className='pt-3'>
-    <h2 className='lg:text-lg font-semibold mb-1'>
-    Logo Design
-    </h2>
-    <ProgressBar percentage={85} fillerExtraStyles={{backgroundColor:'#facc15',}} progressBarExtraStyles={{backgroundColor:'#d1d5db', height:'25px' ,width:'100%'}}/>
-    </div>
-    <div className='pt-3'>
-    <h2 className='lg:text-lg font-semibold mb-1' >
-    Brand Design
-    </h2>
-    <ProgressBar percentage={80} fillerExtraStyles={{backgroundColor:'#facc15',}} progressBarExtraStyles={{backgroundColor:'#d1d5db', height:'25px' ,width:'100%'}}/>
-    </div>
-    <div className='pt-3'>
-    <h2 className='lg:text-md font-semibold mb-1'>
-    Advertising
-    </h2>
-    <ProgressBar percentage={80} fillerExtraStyles={{backgroundColor:'#facc15',}} progressBarExtraStyles={{backgroundColor:'#d1d5db', height:'25px' ,width:'100%'}}/>
-    </div>
-    </div>
+  );
+};
 
-    <div className="w-full  md:mx-2  p-6 border mt-9 mb-5 lg:mt-4 border-yellow-400 rounded-lg shadow-lg">
-      <h2 className="text-xl font-bold mb-4 text-center">Contact Me</h2>
-      <form onSubmit={onSubmit}>
-        <div className="mb-4">
-          <input
-            type="text"
-            name="name"
-            placeholder="Your Name"
-            required
-            className="w-full p-3 border text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400"
-          />
-          
-        </div>
-
-        <div className="mb-4">
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email"
-            required
-            className="w-full p-3 border text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400"
-          />
-         
-        </div>
-
-        <div className="mb-4">
-          <textarea
-            name="message"
-            placeholder="Your Message"
-            className="w-full p-3 border text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400"
-           required
-          />
-         
-        </div>
-
-        <button
-          type="submit"
-          className={`w-full py-3 bg-yellow-400 text-white font-bold rounded-md `}
-          
-        >
-          Submit
-        </button>
-      </form>
-      <span className='text-green-300'>{result}</span>
-    </div>
-    </div>
-    </div>
-    
-  )
-}
-
-export default Skill
+export default Skill;
